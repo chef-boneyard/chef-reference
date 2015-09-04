1351b480e734a3f46b5037d2683394e1b0db97c0
jtimberman <joshua@chef.io>
2015-09-04 11:56:41 -0600

    use bundler for unit phase

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -3,4 +3,19 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-include_recipe 'delivery-truck::unit'
+#
+# we'd use delivery-truck, but we need to bundle install so we can
+# have chefspec that supports policyfiles.
+changed_cookbooks.each do |cookbook|
+  execute "bundle install #{cookbook.name}" do
+    cwd cookbook.path
+    command 'bundle install --path=vendor --binstubs'
+    only_if { ::File.exist?(File.join(cookbook.path, 'Gemfile'))  }
+  end
+
+  execute "unit_rspec_#{cookbook.name}" do
+    cwd cookbook.path
+    command 'bundle exec rspec --format documentation --color'
+    only_if { has_spec_tests?(cookbook.path) }
+  end
+end
