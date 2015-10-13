7566efd0bf1efc64ec7e977517b54a2e1227fbf0
jtimberman <joshua@chef.io>
2015-10-13 10:23:24 -0700

    unit doesnt need bundler now

--- .delivery/build-cookbook/recipes/unit.rb
+++ .delivery/build-cookbook/recipes/unit.rb
@@ -4,18 +4,15 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 #
-# we'd use delivery-truck, but we need to bundle install so we can
-# have chefspec that supports policyfiles.
+
 changed_cookbooks.each do |cookbook|
-  execute "bundle install #{cookbook.name}" do
+  execute 'chef install' do
     cwd cookbook.path
-    command 'bundle install --path=vendor --binstubs'
-    only_if { ::File.exist?(File.join(cookbook.path, 'Gemfile'))  }
   end
 
-  execute "unit_rspec_#{cookbook.name}" do
+  execute 'chef update' do
     cwd cookbook.path
-    command 'bundle exec rspec --format documentation --color'
-    only_if { has_spec_tests?(cookbook.path) }
   end
 end
+
+include_recipe 'delivery-truck::unit'
