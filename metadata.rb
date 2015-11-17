0c5e6cfd22423b6faab5c8e09ac140161ed0c2a5
jtimberman <joshua@chef.io>
2015-11-17 10:44:38 -0700

    Let's not skip the provision phase.
    So we can, you know. Provision. Like we meant to.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'eng-services@chef.io'
 license 'apache2'
 description 'Reference architecture cookbook for Chef products'
-version '0.4.1'
+version '0.4.2'
 
 depends 'chef-ingredient', '>= 0.13.1'
 
